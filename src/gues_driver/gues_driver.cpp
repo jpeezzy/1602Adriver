e7c3b95f4eebcfbbdad3b3575a3505b48db97706
Your Name <jpeezzy@hotmail.com>
2019-02-18 21:02:45 -0800

    stuck in hot swapping

--- src/gues_driver/gues_driver.cpp
+++ src/gues_driver/gues_driver.cpp
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <pins_arduino.h>
-//Found This value after scanning
+
 #define RED_PIN 5
 #define GREEN_PIN 6
 #define BLUE_PIN 7
@@ -15,14 +15,16 @@
 #define CYAN 6
 #define WHITE 7
 
-#define DISK1 0x50 //Address of 24LC256 EEPROM chip
-LiquidCrystal_I2C lcd(0x3F, 16, 2); // set the LCD address to 0x27 for a 16 chars 
-									//and 2 line display
+#define CONNECTED_PIN A7 //Interrupt Pin
+#define MAX_VOLTAGE_READING 5 //Max Voltage 
+#define SWAP_GND 8 //NO CARTRIDGE GND
+#define INTERRUPT_PIN INT5 //check if catridge is pulled out
 
-#define CONNECTED_PIN 11 //Pin in the arduino mega to check if the 
-						 //cartridge is connected or not. periodically check. 
-#define MAX_VOLTAGE_READING 20.0 //max voltage reading for arduino mega
-#define INTERRUPT_PIN 21 //check if catridge is pulled out
+#define DISK1 0x50 //Address of 24LC256 EEPROM chip
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // set the LCD address to 0x27 for a 16x2 display
+volatile byte CARTRIDGE_INSERTED = HIGH;
+int C_LOOP = 0;
 
 void set_lcd_color(unsigned int COLOR);
 void writeEEPROM(int deviceaddress, unsigned int eeaddress, 
@@ -37,10 +39,9 @@ void power_off(); // saves the state of the pots
 
 void hot_swap(); //allows for hot swapping game catridges: requirements- must use arduino mega 
 
-
-
 void setup() 
 {
+	//Serial.begin(9600);
 	Wire.begin(); //enables the pullup resistors in sda scl
 	lcd.init();
 	lcd.backlight();
@@ -59,13 +60,13 @@ void setup()
 	unsigned int address = 0;
 	//String that is sent to the EEPROM, must be less than 
 	//64 characters
-	char* PEDAL_DATA= (char*)("FUZZ/2/RED");
+	//char* PEDAL_DATA= (char*)("FUZZ/2/RED");
 	//writeEEPROM(DISK1, address, PEDAL_DATA);
 
 	//PIN 3 is WP, so after writing data, set if off.
 	digitalWrite(3,HIGH);
 
-	char buf[64] = {0};
+	char buf[16] = {0};
 	readEEPROM(DISK1, address, (char*)buf, 10);
 	lcd.print((char*)buf);
 	delay(300);
@@ -78,12 +79,24 @@ void setup()
 	//initialize catridge checker pin 
 	//pinMode(CONNECTED_PIN, INPUT);
 	//attach interrupt
-	attachInterrupt(INTERRUPT_PIN, hot_swap, CHANGE);
+	//pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+	attachInterrupt(INTERRUPT_PIN, hot_swap, LOW);
 }
 
 void loop() 
 {
-	//lcd.print("HELLO WORLD");
+	if((CARTRIDGE_INSERTED == 0) && (C_LOOP == 0))
+	{
+		set_lcd_color(WHITE);
+		C_LOOP = 1;
+		lcd.clear();
+		delay(100);
+		char cstr[10] = "INSERT";
+		lcd.print(cstr);
+		delay(100);
+		pinMode(SWAP_GND, OUTPUT);
+		digitalWrite(SWAP_GND,LOW);
+	}
 }
 
 void writeEEPROM(int deviceaddress, unsigned int eeaddress, 
@@ -119,10 +132,9 @@ void readEEPROM(int deviceaddress, unsigned int eeaddress,
 void set_lcd_color(unsigned int color)
 {
 	if(color>=1 && color<8){
-//		printf("%d", (((color & 0b010) > 1)));
-		digitalWrite(RED_PIN, (((color & 0b001) == 1) ? HIGH:LOW));
-		digitalWrite(GREEN_PIN, (((color & 0b010) == 2) ? HIGH:LOW));
-		digitalWrite(BLUE_PIN, (((color & 0b100) == 4) ?  HIGH:LOW));
+		digitalWrite(RED_PIN, (((color & 1) == 1) ? HIGH:LOW));
+		digitalWrite(GREEN_PIN, (((color & 2) == 2) ? HIGH:LOW));
+		digitalWrite(BLUE_PIN, (((color & 4) == 4) ?  HIGH:LOW));
 		delay(100);
 	}
 	return;
@@ -130,15 +142,15 @@ void set_lcd_color(unsigned int color)
 
 void hot_swap()
 {
-	//to detect if the user disconnected the cartridge, 
-	//place resistors next to cartridge and check voltage 
-	int slot_voltage = analogRead(CONNECTED_PIN);
-	float voltage = slot_voltage * (MAX_VOLTAGE_READING)/1023.0;
-	if(voltage < 0.1) //some arbitrary threshold 
+	unsigned int slot_voltage = analogRead(CONNECTED_PIN);
+	float voltage = slot_voltage * 5.0/1023.0;
+	if(voltage < 1.0)
 	{
-		//turn off the slot to prevent accidents? 
+		CARTRIDGE_INSERTED = LOW;
 	}
-
-	
-//	return;	
+	else 
+	{
+		CARTRIDGE_INSERTED = HIGH;
+	}
+	return;	
 }
